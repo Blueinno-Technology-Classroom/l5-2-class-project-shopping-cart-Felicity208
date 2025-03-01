82a10d80e4b8b028ad57380bf5c0b438d986784c
Felicity <felicity.curreem@gmail.com>
2025-03-01 12:11:13 +0800

    L5 - 2

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 class Order:
-    # TODO:
-    pass
+    def __init__(self, quantity, price):
+        self.quantity = quantity
+        self.price = price
+        self.cost = 0
+    
+    def get_cost(self):
+        self.cost = self.price * self.quantity
+        print(f'Total: {self.cost}')
+        return self.cost
 
 
 class ShoppingCart:
-    # TODO:
-    pass
+    def __init__(self):
+        self.shoppinglist = []
+        
+    def add(self, order):
+        self.shoppinglist.append(order)
+
+
+    def get_total_cost(self):
+        self.totalCost = 0
+        for i in self.shoppinglist:
+            self.totalCost += i.get_cost()
+        return self.totalCost
 
 
 cart = ShoppingCart()
@@ -14,6 +31,7 @@ banana = Order(2, 4.99)
 
 cart.add(apple)
 cart.add(banana)
+
 
 total = cart.get_total_cost()
 
